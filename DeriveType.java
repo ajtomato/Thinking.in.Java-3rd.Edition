e7eeca6db06d5607e2fab7a948e436204c9ca1d5
ajtomato <ajtomato@gmail.com>
2015-08-10 09:40:31 +0800

    Override must provide more access level.

--- DeriveType.java
+++ DeriveType.java
@@ -3,5 +3,9 @@ class DeriveType extends BaseType {
     DeriveType() {
         System.out.println("DeriveType.Constructor");
     }
-    
+
+    @Override
+    public void protectedMethod() {
+        System.out.println("DeriveType.protectedMethod");
+    }
 }
